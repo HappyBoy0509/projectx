89bf9cf23b45f92bc3804f84319ccd77aced9bf3
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-19 14:58:34 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -113,19 +113,6 @@ if __name__ == "__main__" :
         filters=Filters.command(["Help"]) 
     )
     app.add_handler(help_text_handler)
-   
-   
-    #
-    group_new_join_handler = MessageHandler(
-        help_message_f,
-        filters=Filters.chat & Filters.new_chat_members
-    )
-    app.add_handler(group_new_join_handler)
-    #
-    call_back_button_handler = CallbackQueryHandler(
-        button
-    )
-    app.add_handler(call_back_button_handler)
     #
     save_thumb_nail_handler = MessageHandler(
         save_thumb_nail,
