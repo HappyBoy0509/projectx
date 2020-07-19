a88f433299e9734e21b1b262f7ee0bba73a06ad1
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-19 15:18:32 +0530

    Update __main__.py

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -86,7 +86,7 @@ if __name__ == "__main__" :
     #
     cancel_message_handler = MessageHandler(
         cancel_message_f,
-        filters=Filters.command(["CANCEL"]) 
+        filters=Filters.command("CANCEL") 
     )
     app.add_handler(cancel_message_handler)
     #
